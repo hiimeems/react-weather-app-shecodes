d5854a3b9dc5c5bd9b557d84472e9729329c399b
himeems <michelle@michelles-mbp.home>
2020-11-01 11:03:50 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>weather app</h1>
-      <footer>
-        this project was coded by michelle chung and is{" "}
-        <a
-          href="https://github.com/hiimeems/react-weather-app-shecodes"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>weather app</h1>
+        <footer>
+          this project was coded by michelle chung and is{" "}
+          <a
+            href="https://github.com/hiimeems/react-weather-app-shecodes"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
