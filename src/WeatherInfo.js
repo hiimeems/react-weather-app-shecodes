2434a23e44e1e9e2e3b4d99e90718ff0877a797d
himeems <michelle@michelles-mbp.home>
2020-11-05 12:29:03 -0500

    Updating weather app

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import CityTemperature from "./CityTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -8,33 +9,24 @@ export default function WeatherInfo(props) {
       <h1 className="text-lowercase">
         <span className="city-entered">{props.data.city}</span>
       </h1>
-      <div className="row">
-        <div className="col-4">
-          <div className="clear-fix">
-            <div className="temperature">
-              <span className="float-left">
-                {" "}
-                {Math.round(props.data.temperature)}
-              </span>
-            </div>
-            <div className="float-right">
-              <span className="unit">°C</span>
-            </div>
+      <div className="clear-fix">
+        <div className="row">
+          <div className="col-4">
+            <CityTemperature celsius={props.data.temperature} />
           </div>
-        </div>
-        <div className="col-4">
-          <WeatherIcon code={props.data.icon}  />
-        </div>
-
-        <div className="col-4">
-          <ul>
-            <li>
-              <FormattedDate date={props.data.date} />
-            </li>
-            <li>{props.data.description}</li>
-            <li>wind: {props.data.wind} km/h</li>
-            <li>humidity: {props.data.humidity}%</li>
-          </ul>
+          <div className="col-4">
+            <WeatherIcon code={props.data.icon} />
+          </div>
+          <div className="col-4">
+            <ul>
+              <li>
+                <FormattedDate date={props.data.date} />
+              </li>
+              <li>{props.data.description}</li>
+              <li>wind: {props.data.wind} km/h</li>
+              <li>humidity: {props.data.humidity}%</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
