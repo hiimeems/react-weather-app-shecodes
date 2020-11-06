73f5c4252fede23e484d90a864288f14b7f03b56
himeems <michelle@michelles-mbp.home>
2020-11-06 15:46:02 -0500

    Added forecast component JS & CSS, updated weather app

---
+++ src/WeatherForecast.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./WeatherForecast.css";
+import ForecastPreview from "./ForecastPreview";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function showForecast(response) {
+    setForecast(response.data);
+    setLoaded(true);
+    console.log(response.data);
+  }
+
+  if (loaded && props.city === forecast.city.name) {
+    return (
+      <div className="WeatherForecast row">
+        <ForecastPreview data={forecast.list[0]} />
+        <ForecastPreview data={forecast.list[1]} />
+        <ForecastPreview data={forecast.list[2]} />
+        <ForecastPreview data={forecast.list[3]} />
+        <ForecastPreview data={forecast.list[4]} />
+        <ForecastPreview data={forecast.list[5]} />
+      </div>
+    );
+  } else {
+    let units = "metric";
+    let apiKey = "6e655c51885a817c91e42c6e1aa56edf";
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(showForecast);
+    return null;
+  }
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -9,24 +9,26 @@ export default function WeatherInfo(props) {
       <h1 className="text-lowercase">
         <span className="city-entered">{props.data.city}</span>
       </h1>
-      <div className="clear-fix">
-        <div className="row">
-          <div className="col-4">
-            <CityTemperature celsius={props.data.temperature} />
+      <div className="row">
+        <div className="col-6">
+          <div className="clear-fix">
+            <div className="float-left">
+              <CityTemperature celsius={props.data.temperature} />
+            </div>
+            <div className="float-right">
+              <WeatherIcon code={props.data.icon} />
+            </div>
           </div>
-          <div className="col-4">
-            <WeatherIcon code={props.data.icon} />
-          </div>
-          <div className="col-4">
-            <ul>
-              <li>
-                <FormattedDate date={props.data.date} />
-              </li>
-              <li>{props.data.description}</li>
-              <li>wind: {props.data.wind} km/h</li>
-              <li>humidity: {props.data.humidity}%</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li>{props.data.description}</li>
+            <li>wind: {props.data.wind} km/h</li>
+            <li>humidity: {props.data.humidity}%</li>
+          </ul>
         </div>
       </div>
     </div>
