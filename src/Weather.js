7763105ae5c86210301bb4c29b753487c895eeae
himeems <michelle@michelles-mbp.home>
2020-11-08 09:56:43 -0500

    Imported React Loader Spinner

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import "./Weather.css";
+import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
@@ -86,6 +87,14 @@ export default function Weather(props) {
     );
   } else {
     search();
-    return "Loading...";
+    return (
+      <Loader
+        type="Hearts"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    );
   }
 }
