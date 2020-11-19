036f304d4d7d081a3b51e87bd11782c2891ffa44
himeems <michelle@michelles-MacBook-Pro.local>
2020-11-19 11:42:24 -0800

    Resizing weather icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="blueviolet"
-      size={60}
+      size={70}
       animate={true}
     />
   );
